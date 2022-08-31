808ee68b57dcc9f60e42c4edcc1904b574b3d536
GMohammadAli <gulammohammadali@gmail.com>
2022-08-31 13:22:59 +0530

    UseRef Example 1 Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UseRefExample1 from './components/UseRefExample1';
+
+function App() {
+  return (
+    <div>
+      <UseRefExample1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UseRefExample1.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+
+function UseRefExample1() {
+    const inputRef = useRef();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputRef.current);
+        document.getElementById('h1').innerText = inputRef.current.value;    
+        inputRef.current.focus();   
+        inputRef.current.value=""
+    }
+  return (
+    <div className="container mt-5">
+      <h1 id="h1">Demo use of useRef Hook for creating DOM reference</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name
+        </label>
+        <input type="text" id="name" ref={inputRef} placeholder="Name" className='form-control mb-2'/>
+        <button type="submit" className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default UseRefExample1
